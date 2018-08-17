d7f15d9e9e99dfe5554ac93ec5c9040df06ed390
rupadevivaragana@gmail.com <rupa@6436>
2018-08-17 04:14:25 -0500

    added
    files

---
+++ javascript-master/00_HTML_CSS_JS/app.js
@@ -0,0 +1,15 @@
+function blue() {
+    document.getElementById('myStyles').setAttribute('href', 'blue.css');
+
+}
+function green() {
+    document.getElementById('myStyles').setAttribute('href', 'green.css');
+
+}
+function red() {
+    document.getElementById('myStyles').setAttribute('href', 'red.css');
+}
+function yellow() {
+    document.getElementById('myStyles').setAttribute('href', 'yellow.css');
+
+}
